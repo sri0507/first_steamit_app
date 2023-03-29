8105330ed9ec014a68eabbc05cf265b38cfe6ad7
sri0507 <129300578+sri0507@users.noreply.github.com>
2023-03-29 13:32:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥣 🥗 🐔 🥑🍞Kale spinach & Rocket smoothie')
+streamlit.text('🥣 🥗 🐔 🥑🍞 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 🥗 🐔 🥑🍞 Kale spinach & Rocket smoothie')
 streamlit.text('🥣 🥗 🐔 🥑🍞 Hard-Boiled Free-Range Egg' )
-streamlit.text('🥣 🥗 🐔 🥑🍞 Avocado toast
+streamlit.text('🥣 🥗 🐔 🥑🍞 Avocado toast')
